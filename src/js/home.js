24235e60cc07ca9d4b9d52e47053a6e745c495a9
weisitong <admin@weyston.com>
2025-11-29 17:26:29 +0800

    Update home.js

--- src/js/home.js
+++ src/js/home.js
@@ -193,7 +193,7 @@ const hotSellingModels = [
         brand_zh: "太阳诱电 太诱",
         category_en: "MLCC Capacitor",
         category_zh: "多层陶瓷电容",
-        model: "LDK063BBJ225MP-F",
+        model: "LDK063BBJ225MPLF",
         specs: "",
         feature_en: "Best price, authorized channel, stable supply",
         feature_zh: "价格最优 代理渠道 供应稳定",
